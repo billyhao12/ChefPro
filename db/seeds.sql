635e9925688bda22122559ef0895b06be3874ad0
Billy Hao <billyhao12@gmail.com>
2020-07-11 14:22:30 -0700

    Added a seeds.sql file to the db directory with 3 recipes

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO recipes (title, author, ingredients, instructions, UserId)
+VALUES
+    ("Fruit Salad", "John Smith", "Lettuce, Tomatoes, Strawberries, Cheese, Thousand Island dressing", "Put some lettuce in a bowl. Then add cheese and dressing. Finally, sprinkle on tomatoes and strawberries.", 1),
+
+    ("Waffles", "Tom Hanks", "Waffles, Syrup", "Heat waffles in toaster for 3 minutes. Then, place them in a plate and add syrup!", 1),
+
+    ("Instant Noodles", "Billy Joel", "Buy an instant noodles from the supermarket", "Add seasoning to the noodles. Then, add hot water and wait for 2 minutes. Finally, enjoy your meal!", 1);
