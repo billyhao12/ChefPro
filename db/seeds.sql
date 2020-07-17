6b002cfc9d4939d7b8ced4ab60600f0865e088ff
Aidan Q <aidanq52@gmail.com>
2020-07-16 19:22:39 -0700

    changing the route from login

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,9 @@
-INSERT INTO recipes (title, author, ingredients, instructions, UserId)
-VALUES
-    ("Fruit Salad", "John Smith", "Lettuce, Tomatoes, Strawberries, Cheese, Thousand Island dressing", "Put some lettuce in a bowl. Then add cheese and dressing. Finally, sprinkle on tomatoes and strawberries.", 1),
+INSERT INTO users (email, password, createdAt, updatedAt)
+VALUES ("mail@mail.com", "password", "2020-07-11", "2020-07-11"),
+("whatsup@snail.com", "password2", "2020-07-01", "2020-07-02");
 
-    ("Waffles", "Tom Hanks", "Waffles, Syrup", "Heat waffles in toaster for 3 minutes. Then, place them in a plate and add syrup!", 1),
-
-    ("Instant Noodles", "Billy Joel", "Buy an instant noodles from the supermarket", "Add seasoning to the noodles. Then, add hot water and wait for 2 minutes. Finally, enjoy your meal!", 1);
+INSERT INTO recipes (title, author, ingredients, instructions, createdAt, updatedAt, UserId)
+VALUES 
+	("salad", "Billy", "leaves, strawberries", "mix everything together", "2020-07-09", "2020-07-09", 1),
+	("Waffles", "Tom Hanks", "Waffles, Syrup", "Heat waffles in toaster for 3 minutes. Then, place them in a plate and add syrup!", "2020-07-10", "2020-07-09", 1),
+    ("Instant Noodles", "Billy Joel", "Buy an instant noodles from the supermarket", "Add seasoning to the noodles. Then, add hot water and wait for 2 minutes. Finally, enjoy your meal!", "2020-07-11", "2020-07-09", 2);
